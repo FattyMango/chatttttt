5497212e9ef95f5bb32db2bd8abd3dcd00b03475
fattymango <malak.jkl@gmial.com>
2020-12-30 16:43:55 +0200

    Update settings.py

--- chattest/settings.py
+++ chattest/settings.py
@@ -136,7 +136,9 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn')
 TEMP = os.path.join(BASE_DIR,'media_cdn/temp')
