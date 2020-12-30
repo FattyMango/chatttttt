fbb87bcc0d97f3b23292b72d2f12bba21e014932
fattymango <malak.jkl@gmial.com>
2020-12-30 15:39:26 +0200

    cn

--- chattest/settings.py
+++ chattest/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,4 +129,14 @@ ASGI_APPLICATION = 'chattest.asgi.application'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'media'),
+
+]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn')
+TEMP = os.path.join(BASE_DIR,'media_cdn/temp')
+BASE_URL = 'https//:127.0.0.1:8000'
--- chattest/urls.py
+++ chattest/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
 ]
+if settings.DEBUG :
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
